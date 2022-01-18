8a8309e33faac31ea24993a739d44d4e65ee432b
Kat Shambaugh <44981851+KatShambaugh@users.noreply.github.com>
2022-01-18 16:22:35 -0500

    Delete Interweb directory

--- Interweb/Game Files/js-footer.js
+++
@@ -1,9 +0,0 @@
-document.write('<br>Katie\'s Super Cool Site - Copyright &copy; 2000<br>');
-document.write('All images, trademarks, and copyrights are property of their respective owners.<br><br>');
-
-/* Buttons */
-document.write('<a href="http://homeandlearn.co.uk/WD/WebDesign.html"><img src="img/html.gif" alt="[Learn HTML now!]" title="Learn HTML Now!"><\/a>');
-document.write('<a href="http://jigsaw.w3.org/css-validator/check/referer"><img src="img/css.gif" alt="[Made with Cascading Style Sheets]" title="Made with Cascading Style Sheets"><\/a>');
-document.write('<img src="img/windowsbutton.gif" alt="[Made with Windows]" title="Made with Windows">');
-document.write('<a href="//www.neocities.org"><img src="img/neocities.gif" alt="[Hosted by NeoCities. Get your own free website today!]" title="Hosted by NeoCities. Get your own free website today!"><\/a>');
-document.write('<a href="http://notepad-plus-plus.org/"><img src="img/notepadpp.gif" alt="[Made with Notepad++]" title="Made with Notepad++"><\/a>');
